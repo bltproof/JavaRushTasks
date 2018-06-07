0391c9af3f46f42f8332846b0fb10077543df315
bltproof <aka.vincente.vega@gmail.com>
2018-06-07 03:57:56 +0300

    Файлы и Исключения 1824

--- 2.JavaCore/src/com/javarush/task/task18/task1824/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1824/Solution.java
@@ -4,7 +4,19 @@ package com.javarush.task.task18.task1824;
 Файлы и исключения
 */
 
+import java.io.*;
+
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            while (true) {
+                String fileName = reader.readLine();
+                try (FileReader fileReader = new FileReader(fileName)) {
+                } catch (FileNotFoundException e) {
+                    System.out.println(fileName);
+                    break;
+                }
+            }
+        }
     }
 }
