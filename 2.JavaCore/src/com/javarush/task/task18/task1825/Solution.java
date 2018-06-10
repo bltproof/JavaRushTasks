cbd8aeaf6cc92c4041a2e52e6b4f4fabda5f7458
bltproof <aka.vincente.vega@gmail.com>
2018-06-10 17:06:56 +0300

    both kind of control

--- 2.JavaCore/src/com/javarush/task/task18/task1825/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1825/Solution.java
@@ -5,6 +5,7 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.Buffer;
 import java.util.Comparator;
 import java.util.Set;
 import java.util.TreeSet;
@@ -14,6 +15,11 @@ import java.util.TreeSet;
 */
 
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String input = reader.readLine();
+        try (BufferedReader fis = new BufferedReader(new InputStreamReader(new FileInputStream(input)))) {
+
+        }
     }
 }
