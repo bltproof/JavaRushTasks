fbfbf31ec0cfa6a49a8f307cebaea6bf3aa5ebac
bltproof <aka.vincente.vega@gmail.com>
2018-06-12 05:01:22 +0300

    Собираем файл

--- 2.JavaCore/src/com/javarush/task/task18/task1825/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1825/Solution.java
@@ -1,14 +1,7 @@
 package com.javarush.task.task18.task1825;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.Buffer;
-import java.util.Comparator;
-import java.util.Set;
-import java.util.TreeSet;
+import java.io.*;
+import java.util.*;
 
 /* 
 Собираем файл
@@ -16,10 +9,37 @@ import java.util.TreeSet;
 
 public class Solution {
     public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String input = reader.readLine();
-        try (BufferedReader fis = new BufferedReader(new InputStreamReader(new FileInputStream(input)))) {
-
+        Set<String> filesTreeSet = new TreeSet<>(new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                int a = Integer.parseInt(o1.split("part")[1]);
+                int b = Integer.parseInt(o2.split("part")[1]);
+                return a - b;
+            }
+        });
+        String fileName;
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            fileName = reader.readLine();
+            while (!"end".equals(fileName)) {
+                filesTreeSet.add(fileName);
+                fileName = reader.readLine();
+            }
         }
+        String fileToWrite = "";
+        FileOutputStream fileOutputStream = null;
+        for (String setElement : filesTreeSet) {
+            if (!setElement.contains(fileToWrite) || fileToWrite.equals("")) {
+                fileToWrite = setElement.substring(0, setElement.lastIndexOf('.'));
+                fileOutputStream = new FileOutputStream(fileToWrite);
+            }
+            FileInputStream fileInputStream = new FileInputStream(setElement);
+            byte[] buffer = new byte[fileInputStream.available()];
+            while (fileInputStream.available() > 0) {
+                int readed = fileInputStream.read(buffer);
+                fileOutputStream.write(buffer, 0, readed);
+            }
+            fileInputStream.close();
+        }
+        if (fileOutputStream != null) fileOutputStream.close();
     }
 }
