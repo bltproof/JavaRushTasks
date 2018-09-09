2c5aead4b7c478a0856ae95342f77521606a5195
bltproof <aka.vincente.vega@gmail.com>
2018-09-09 23:38:18 +0300

    upd

--- 2.JavaCore/src/com/javarush/task/task19/task1906/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1906/Solution.java
@@ -22,9 +22,9 @@ public class Solution {
 
     private static void readFile(String fileNameRead) {
         try (FileReader fileReader = new FileReader(fileNameRead)) {
-            int c;
-            while ((c = fileReader.read()) != -1) {
-                charList.add((char) c);
+            int readed;
+            while ((readed = fileReader.read()) != -1) {
+                charList.add((char) readed);
             }
         } catch (IOException ex) {
             ex.printStackTrace();
@@ -33,11 +33,12 @@ public class Solution {
 
     private static void writeFile(String fileNameWrite) {
         try (FileWriter fileWriter = new FileWriter(fileNameWrite)) {
-            int length = charList.size() + 1;
-            for (int i = 0; i < length; i++) {
-                if ((length - i) % 2 == 0) {
-                    fileWriter.write(charList.get(i));
+            int count = 1;
+            for (Character ch : charList) {
+                if (count % 2 == 0) {
+                    fileWriter.write(ch);
                 }
+                count++;
             }
         } catch (IOException ex) {
             ex.printStackTrace();
