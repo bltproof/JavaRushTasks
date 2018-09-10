d624e7a95b3c5444c283715cdb25aa4e45a5c72d
ShaipovZA <z.shaipov@id-mt.ru>
2018-09-10 23:02:41 +0300

    task 1908

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1908/Solution.java
@@ -0,0 +1,29 @@
+package com.javarush.task.task19.task1908;
+
+/* 
+Выделяем числа
+*/
+
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+             BufferedReader fileReader = new BufferedReader(new FileReader(reader.readLine()));
+             BufferedWriter fileWriter = new BufferedWriter(new FileWriter(reader.readLine()))) {
+
+            String[] line = fileReader.readLine().split(" ");
+            for (String element : line) {
+                try {
+                    int number = Integer.parseInt(element);
+                    fileWriter.write(number + " ");
+
+                } catch (Exception ex) {
+                    continue;
+                }
+            }
+        }
+    }
+}
+
