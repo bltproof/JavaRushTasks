13be295f74d79b45d6384101e6900ad261e77869
bltproof <aka.vincente.vega@gmail.com>
2018-09-14 11:45:05 +0300

    upd

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1909/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.task19.task1909;
+
+/* 
+Замена знаков
+*/
+
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+             BufferedReader fileReader = new BufferedReader(new FileReader(reader.readLine()));
+             BufferedWriter fileWriter = new BufferedWriter(new FileWriter(reader.readLine()))) {
+
+            while (fileReader.ready()) {
+                fileWriter.write(fileReader.readLine().replaceAll("\\.", "!"));
+            }
+        }
+    }
+}
