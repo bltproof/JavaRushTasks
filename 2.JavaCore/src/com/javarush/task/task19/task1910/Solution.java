afe0f4d43da6bb55808e772bb784f7ae40d33abc
bltproof <aka.vincente.vega@gmail.com>
2018-09-17 00:03:37 +0300

    upd

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1910/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.task19.task1910;
+
+/* 
+Пунктуация
+*/
+
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+             BufferedReader fileReader = new BufferedReader(new FileReader(reader.readLine()));
+             BufferedWriter fileWriter = new BufferedWriter(new FileWriter(reader.readLine()))) {
+
+            while (fileReader.ready()) {
+                fileWriter.write(fileReader.readLine().replaceAll("[\\p{Punct}]", ""));
+            }
+        }
+    }
+}
