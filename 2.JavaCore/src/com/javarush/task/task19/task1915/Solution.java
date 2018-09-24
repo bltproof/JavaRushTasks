2c0e3f8ac5fa751ccf4a868aacd049d9062ce073
bltproof <aka.vincente.vega@gmail.com>
2018-09-24 23:23:29 +0300

    upd

--- 2.JavaCore/src/com/javarush/task/task19/task1915/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1915/Solution.java
@@ -4,10 +4,26 @@ package com.javarush.task.task19.task1915;
 Дублируем текст
 */
 
+import java.io.*;
+
 public class Solution {
     public static TestString testString = new TestString();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+             FileOutputStream fileOutputStream = new FileOutputStream(reader.readLine())) {
+
+            PrintStream consolreader = System.out;
+            ByteArrayOutputStream byteArr = new ByteArrayOutputStream();
+            PrintStream printStream = new PrintStream(byteArr);
+            PrintStream printFile = new PrintStream(fileOutputStream);
+            System.setOut(printStream);
+            testString.printSomething();
+            System.setOut(consolreader);
+            String s = byteArr.toString();
+            printFile.println(s);
+            System.out.println(s);
+        }
     }
 
     public static class TestString {
