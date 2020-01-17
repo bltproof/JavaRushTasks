e9c3fa3d3e2cff6cac717747394da8b449cdb0f2
bltproof <aka.vincente.vega@gmail.com>
2020-01-17 06:10:01 +0300

    длинные слова

--- 2.JavaCore/src/com/javarush/task/task19/task1925/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1925/Solution.java
@@ -4,8 +4,41 @@ package com.javarush.task.task19.task1925;
 Длинные слова
 */
 
-public class Solution {
-    public static void main(String[] args) {
+import java.io.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        String[] array;
+        StringBuilder lines = new StringBuilder();
+
+        try (BufferedReader fileReader = new BufferedReader(new FileReader(args[0]))) {
+            while (fileReader.ready()) {
+                array = fileReader.readLine().split("\\s");
+                for (String line : array) {
+                    lines.append(line);
+                    lines.append(" ");
+                }
+            }
+        }
+
+        List<String> list = Arrays.stream(lines.toString().split("\\s"))
+                .filter(s -> s.length() > 6)
+                .collect(Collectors.toList());
+
+        try (BufferedWriter fileWriter = new BufferedWriter(new FileWriter(args[1]))) {
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < list.size(); i++) {
+                sb.append(list.get(i));
+                if ((list.size() - 1) - i == 0) {
+                    break;
+                }
+                sb.append(",");
+            }
+            fileWriter.write(sb.toString());
+        }
     }
 }
--- 2.JavaCore/src/com/javarush/task/task19/task1926/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1926/Solution.java
@@ -4,7 +4,32 @@ package com.javarush.task.task19.task1926;
 Перевертыши
 */
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        String fileName;
+
+        try (BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in))) {
+            fileName = consoleReader.readLine();
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        try (BufferedReader fileReader = new BufferedReader(new FileReader(fileName))) {
+            while (fileReader.ready()) {
+                sb.append(fileReader.readLine());
+                sb.append("\n");
+            }
+        }
+        String[] reverse = sb.reverse().toString().split("\n");
+
+        for (int i = reverse.length - 1; i > 0; i--) {
+            System.out.println(reverse[i]);
+        }
     }
 }
