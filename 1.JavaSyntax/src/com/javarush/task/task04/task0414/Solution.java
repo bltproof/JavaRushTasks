f501a85ad8f51b9cea69950284fd49390bc38dbc
bltproof <aka.vincente.vega@gmail.com>
2020-01-17 08:26:21 +0300

    Количество дней в году

--- 1.JavaSyntax/src/com/javarush/task/task04/task0414/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0414/Solution.java
@@ -9,5 +9,16 @@ import java.io.*;
 public class Solution {
     public static void main(String[] args) throws Exception {
         //напишите тут ваш код
+        int num;
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            num = Integer.parseInt(reader.readLine());
+        }
+
+        if (num % 400 == 0 || num % 4 == 0 && num % 100 != 0) {
+            System.out.println("количество дней в году: 366");
+
+        } else {
+            System.out.println("количество дней в году: 365");
+        }
     }
 }
