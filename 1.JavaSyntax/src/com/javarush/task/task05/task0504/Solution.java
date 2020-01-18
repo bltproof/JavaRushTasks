e1d80b0eaed8b6237c756841380dd57d5457810a
bltproof <aka.vincente.vega@gmail.com>
2020-01-18 14:33:50 +0300

    Трикотаж

---
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0504/Solution.java
@@ -0,0 +1,29 @@
+package com.javarush.task.task05.task0504;
+
+
+/* 
+Трикотаж
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        Cat cat = new Cat("Tom", 2, 4, 4);
+        Cat cat2 = new Cat("John", 4, 6, 6);
+        Cat cat3 = new Cat("Elon", 5, 7, 5);
+    }
+
+    public static class Cat {
+        private String name;
+        private int age;
+        private int weight;
+        private int strength;
+
+        public Cat(String name, int age, int weight, int strength) {
+            this.name = name;
+            this.age = age;
+            this.weight = weight;
+            this.strength = strength;
+        }
+    }
+}
