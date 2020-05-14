0c6593fcd669f4236f3bbac8135e3e7b212ace60
bltproof <aka.vincente.vega@gmail.com>
2020-05-14 06:45:03 +0300

    Null Object Pattern

--- 4.JavaCollections/src/com/javarush/task/task31/task3107/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task31/task3107/Solution.java
@@ -1,6 +1,9 @@
 package com.javarush.task.task31.task3107;
 
+import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 /*
 Null Object Pattern
@@ -9,11 +12,24 @@ public class Solution {
     private FileData fileData;
 
     public Solution(String pathToFile) {
+        Path path = Paths.get(pathToFile);
 
+        try {
+
+            fileData = new ConcreteFileData
+                    (
+                            Files.isHidden(path),
+                            Files.isExecutable(path),
+                            Files.isDirectory(path),
+                            Files.isWritable(path)
+                    );
+
+        } catch (IOException ex) {
+            fileData = new NullFileData(ex);
+        }
     }
 
     public FileData getFileData() {
         return fileData;
     }
-
 }
