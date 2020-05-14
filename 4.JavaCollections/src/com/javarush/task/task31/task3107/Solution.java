ad415fe7e92138d39711e402e49909abb9b3787f
bltproof <aka.vincente.vega@gmail.com>
2020-05-14 03:26:22 +0300

    Null Object Pattern

---
+++ 4.JavaCollections/src/com/javarush/task/task31/task3107/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task31.task3107;
+
+import java.nio.file.Files;
+
+/*
+Null Object Pattern
+*/
+public class Solution {
+    private FileData fileData;
+
+    public Solution(String pathToFile) {
+
+    }
+
+    public FileData getFileData() {
+        return fileData;
+    }
+
+}
