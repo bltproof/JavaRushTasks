364e982010ca3859469cc70ea6ee49606a13743b
bltproof <aka.vincente.vega@gmail.com>
2020-05-25 16:15:39 +0300

    Читаем из потока

---
+++ 4.JavaCollections/src/com/javarush/task/task32/task3202/Solution.java
@@ -0,0 +1,41 @@
+package com.javarush.task.task32.task3202;
+
+import java.io.*;
+import java.util.stream.Collectors;
+
+/* 
+Читаем из потока
+*/
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        StringWriter writer = getAllDataFromInputStream(new FileInputStream("/PROJECTS/JavaRushTasks/4.JavaCollections/src/com/javarush/task/task32/task3202/file.txt"));
+        System.out.println(writer.toString());
+    }
+
+    public static StringWriter getAllDataFromInputStream(InputStream is) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(is))) {
+
+            String s = reader.lines().collect(Collectors.joining(System.lineSeparator()));
+
+            StringWriter writer = new StringWriter();
+            writer.write(s);
+            writer.close();
+
+            /*StringBuilder sb = new StringBuilder();
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line);
+            }
+
+            StringWriter writer = new StringWriter();
+            writer.write(sb.toString());
+            writer.close();*/
+
+            return writer;
+
+        } catch (NullPointerException ex) {
+            return new StringWriter();
+        }
+    }
+}
