287651b8158b8532a26ed50c91a783af470f651e
bltproof <aka.vincente.vega@gmail.com>
2020-05-29 22:44:16 +0300

    Шифр Цезаря

---
+++ 4.JavaCollections/src/com/javarush/task/task32/task3213/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.task32.task3213;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.StringReader;
+
+/* 
+Шифр Цезаря
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        StringReader reader = new StringReader("Khoor#Dpljr#&C,₷B'3");
+        System.out.println(decode(reader, -3));  //Hello Amigo #@)₴?$0
+    }
+
+    public static String decode(StringReader reader, int key) throws IOException {
+        try (BufferedReader br = new BufferedReader(reader)) {
+            StringBuilder sb = new StringBuilder();
+            for (Character ch : br.readLine().toCharArray()) {
+                char c = (char) (ch + key);
+                sb.append(c);
+            }
+            return sb.toString();
+
+        } catch (NullPointerException ex) {
+            return "";
+        }
+
+    }
+}
