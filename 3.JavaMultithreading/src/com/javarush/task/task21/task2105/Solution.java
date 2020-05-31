e05180a7c4c1fe29b9872a967768c15a534fc723
bltproof <aka.vincente.vega@gmail.com>
2020-05-31 18:16:31 +0300

    Исправить ошибку. Сравнение объектов

---
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2105/Solution.java
@@ -0,0 +1,39 @@
+package com.javarush.task.task21.task2105;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+/* 
+Исправить ошибку. Сравнение объектов
+*/
+public class Solution {
+    private final String first, last;
+
+    public Solution(String first, String last) {
+        this.first = first;
+        this.last = last;
+    }
+
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!(o instanceof Solution))
+            return false;
+        Solution n = (Solution) o;
+        if (!Objects.equals(first, n.first)) return false;
+        return Objects.equals(last, n.last);
+    }
+
+    public int hashCode() {
+        int result = first != null ? first.hashCode() : 0;
+        result = 31 * result + (last != null ? last.hashCode() : 0);
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Set<Solution> s = new HashSet<>();
+        s.add(new Solution("Mickey", "Mouse"));
+        System.out.println(s.contains(new Solution("Mickey", "Mouse")));
+    }
+}
