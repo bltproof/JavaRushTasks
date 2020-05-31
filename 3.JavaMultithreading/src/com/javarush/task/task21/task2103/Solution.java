e9441888ed1833c0e02033c730ae3abfac105ada
bltproof <aka.vincente.vega@gmail.com>
2020-05-31 17:53:28 +0300

    Все гениальное - просто!

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2103/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2103/Solution.java
@@ -5,9 +5,21 @@ package com.javarush.task.task21.task2103;
 */
 public class Solution {
     public static boolean calculate(boolean a, boolean b, boolean c, boolean d) {
+//        return (a && b && c && !d) || (!a && c) || (!b && c) || (c && d);
         return c;
     }
 
     public static void main(String[] args) {
+        boolean[] bb = {true, false};
+        for (boolean b1 : bb) {
+            for (boolean b2 : bb) {
+                for (boolean b3 : bb) {
+                    for (boolean b4 : bb) {
+
+                        System.out.println(b1 + "," + b2 + "," + b3 + "," + b4 + " = " + calculate(b1, b2, b3, b4));
+                    }
+                }
+            }
+        }
     }
 }
