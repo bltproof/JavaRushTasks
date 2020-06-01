f67d2f5ef08dc39d727ef8d8188faa1705340f9a
ShaipovZA <z.shaipov@id-mt.ru>
2020-06-01 23:27:10 +0300

    Ипподром(3)

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
@@ -1,10 +1,13 @@
 package com.javarush.task.task21.task2113;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Hippodrome {
-    private List<Horse> horses = new ArrayList<>();
+    private List<Horse> horses;
+
+    public Hippodrome(List<Horse> horses) {
+        this.horses = horses;
+    }
 
     public List<Horse> getHorses() {
         return horses;
