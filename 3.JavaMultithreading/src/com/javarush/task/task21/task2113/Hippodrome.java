43801741d0cf670caed2fb2517e0b2d65e36d19d
ShaipovZA <z.shaipov@id-mt.ru>
2020-06-01 23:52:57 +0300

    Ипподром(6)

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
@@ -1,9 +1,14 @@
 package com.javarush.task.task21.task2113;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class Hippodrome {
     private List<Horse> horses;
+    static Hippodrome game;
+
+    public Hippodrome() {
+    }
 
     public Hippodrome(List<Horse> horses) {
         this.horses = horses;
@@ -14,6 +19,11 @@ public class Hippodrome {
     }
 
     public static void main(String[] args) {
-        
+
+        Horse horse1 = new Horse("Horse_1", 3, 0);
+        Horse horse2 = new Horse("Horse_2", 3, 0);
+        Horse horse3 = new Horse("Horse_3", 3, 0);
+
+        game = new Hippodrome(Arrays.asList(horse1, horse2, horse3));
     }
 }
