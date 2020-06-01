4a963fe647d314772fa4c2f476e3bd8a2166e97c
bltproof <aka.vincente.vega@gmail.com>
2020-06-01 15:20:25 +0300

    Запретить клонирование

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2109/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2109/Solution.java
@@ -20,6 +20,30 @@ public class Solution {
         public int getJ() {
             return j;
         }
+
+        @Override
+        protected Object clone() throws CloneNotSupportedException {
+            return super.clone();
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            A a = (A) o;
+
+            if (i != a.i) return false;
+            return j == a.j;
+
+        }
+
+        @Override
+        public int hashCode() {
+            int result = i;
+            result = 31 * result + j;
+            return result;
+        }
     }
 
     public static class B extends A {
@@ -33,15 +57,62 @@ public class Solution {
         public String getName() {
             return name;
         }
+
+        @Override
+        protected Object clone() throws CloneNotSupportedException {
+            if (this instanceof C) {
+                return super.clone();
+            }
+            throw new CloneNotSupportedException();
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            if (!super.equals(o)) return false;
+
+            B b = (B) o;
+
+            return name != null ? name.equals(b.name) : b.name == null;
+
+        }
+
+        @Override
+        public int hashCode() {
+            int result = super.hashCode();
+            result = 31 * result + (name != null ? name.hashCode() : 0);
+            return result;
+        }
     }
 
-    public static class C extends B {
+    public static class C extends B implements Cloneable {
         public C(int i, int j, String name) {
             super(i, j, name);
+        }
+
+        @Override
+        protected Object clone() throws CloneNotSupportedException {
+            return super.clone();
         }
     }
 
     public static void main(String[] args) {
+        try {
+            A a = new A(2, 3);
+            A clonedA = (A) a.clone();
+
+            B b = new B(1, 2, "b");
+            B clonedB = (B) b.clone(); //Exception
+
+            C c = new C(3, 4, "c");
+            C clonedC = (C) c.clone(); //ok
+
+            System.out.println(clonedA == clonedC);
+
+        } catch (CloneNotSupportedException ex) {
+            ex.printStackTrace();
+        }
 
     }
 }
