5110c7b00f09f00421e06b81ee1166e967ccd53c
ShaipovZA <z.shaipov@id-mt.ru>
2020-06-04 02:44:42 +0300

    Найти подстроку

--- 3.JavaMultithreading/src/com/javarush/task/task22/task2202/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2202/Solution.java
@@ -6,12 +6,25 @@ package com.javarush.task.task22.task2202;
 public class Solution {
     public static void main(String[] args) {
         System.out.println(getPartOfString("JavaRush - лучший сервис обучения Java."));
+        System.out.println(getPartOfString("JavaRush - лучший сервис обучения Java Java."));
+        System.out.println(getPartOfString("JavaRush - лучший сервис обучения Java Java Java."));
+        System.out.println(getPartOfString("JavaRush - лучший сервис обучения Java Java Java Java."));
+        System.out.println(getPartOfString("JavaRush - лучший."));
     }
 
     public static String getPartOfString(String string) {
-        return null;
+        String[] array;
+
+        if (string == null || (array = string.split(" ")).length - 1 < 4) {
+            throw new TooShortStringException();
+        }
+
+        if (array.length - 1 > 4) {
+            return string.substring(string.indexOf(" "), string.indexOf(array[4]) + array[4].length()).trim();
+        }
+        return string.substring(string.indexOf(" ")).trim();
     }
 
-    public static class TooShortStringException {
+    public static class TooShortStringException extends RuntimeException {
     }
 }
