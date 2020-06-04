0c7c56e17e5a983e15b75828ed50e173a322212f
ShaipovZA <z.shaipov@id-mt.ru>
2020-06-04 13:59:42 +0300

    Между табуляциями

--- 3.JavaMultithreading/src/com/javarush/task/task22/task2203/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2203/Solution.java
@@ -4,8 +4,29 @@ package com.javarush.task.task22.task2203;
 Между табуляциями
 */
 public class Solution {
-    public static String getPartOfString(String string) {
-        return null;
+    public static String getPartOfString(String string) throws TooShortStringException {
+        try {
+            int tabCount = 0;
+
+            for (int i = 0; i < string.length(); i++) {
+                if (string.charAt(i) == 9) {
+                    tabCount++;
+                }
+            }
+
+            int[] tabIndexArray = new int[tabCount];
+
+            for (int i = 0; i < string.length(); i++) {
+                if (string.charAt(i) == 9) {
+                    tabIndexArray[--tabCount] = i;
+                }
+            }
+
+            return string.substring(tabIndexArray[tabIndexArray.length - 1], tabIndexArray[tabIndexArray.length - 2]).trim();
+
+        } catch (Exception ex) {
+            throw new TooShortStringException();
+        }
     }
 
     public static class TooShortStringException extends Exception {
@@ -13,5 +34,8 @@ public class Solution {
 
     public static void main(String[] args) throws TooShortStringException {
         System.out.println(getPartOfString("\tJavaRush - лучший сервис \tобучения Java\t."));
+        System.out.println(getPartOfString("Начало \tЯвляется - лучшим временем \tконец худшим\t."));
+        System.out.println(getPartOfString("Начало \tНужно для того \t - лучшим \t временем \tконец худшим\t."));
+        System.out.println(getPartOfString("Начало \tНужно для того"));
     }
 }
