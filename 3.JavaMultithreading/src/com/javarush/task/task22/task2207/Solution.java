5d303d2411dfb1a7746a125515b835ed5d3354e5
bltproof <aka.vincente.vega@gmail.com>
2020-06-05 00:34:23 +0300

    Обращенные слова

--- 3.JavaMultithreading/src/com/javarush/task/task22/task2207/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2207/Solution.java
@@ -1,7 +1,10 @@
 package com.javarush.task.task22.task2207;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
 /* 
 Обращенные слова
@@ -10,12 +13,47 @@ public class Solution {
     public static List<Pair> result = new LinkedList<>();
 
     public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in);
+             BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(scanner.nextLine())))) {
 
+            StringBuilder sb = new StringBuilder();
+
+            while (reader.ready()) {
+                sb.append(reader.readLine());
+                sb.append(" ");
+            }
+            List<String> list = new ArrayList<>(Arrays.asList(sb.toString().split("\\s")));
+
+            sb = new StringBuilder();
+            String str = list.get(0);
+
+            for (int i = 1; i < list.size() - 1; i++) {
+                sb.append(str);
+
+                if (sb.reverse().toString().equals(list.get(i))) {
+                    Pair pair = new Pair();
+                    pair.first = str;
+                    pair.second = list.get(i);
+                    if (!result.contains(pair)) {
+                        result.add(pair);
+                    }
+                }
+                str = list.get(i + 1);
+                sb = new StringBuilder();
+            }
+            System.out.println(result);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static class Pair {
         String first;
         String second;
+
+        public Pair() {
+        }
 
         @Override
         public boolean equals(Object o) {
