6354407f56bc84a1f4886821cd453cf0e48e43e3
bltproof <aka.vincente.vega@gmail.com>
2020-06-06 21:31:18 +0300

    Формируем WHERE

--- 3.JavaMultithreading/src/com/javarush/task/task22/task2208/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2208/Solution.java
@@ -1,5 +1,6 @@
 package com.javarush.task.task22.task2208;
 
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 /* 
@@ -7,10 +8,35 @@ import java.util.Map;
 */
 public class Solution {
     public static void main(String[] args) {
+        Map<String, String> map = new LinkedHashMap<>();
+        map.put("name", "Ivanov");
+        map.put("country", "Ukraine");
+        map.put("age", null);
+        map.put("city", "Kiev");
+
+        String s = getQuery(map);
+        System.out.println(s);
 
     }
 
     public static String getQuery(Map<String, String> params) {
-        return null;
+        StringBuilder sb = new StringBuilder();
+
+        for (Map.Entry<String, String> entry : params.entrySet()) {
+            String key = entry.getKey();
+            String value = entry.getValue();
+
+            if (key != null && value != null) {
+                sb.append(key);
+                sb.append(" = '");
+                sb.append(value).append("' ");
+                sb.append("and ");
+            }
+        }
+        if (sb.toString().endsWith("and ")) {
+            sb.delete(sb.length() - 4, sb.length());
+        }
+
+        return sb.toString().trim();
     }
 }
