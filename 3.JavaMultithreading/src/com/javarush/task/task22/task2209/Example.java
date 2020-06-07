5fe2c9da25d4cc78c3e819d8379dc4f23fa60ad5
bltproof <aka.vincente.vega@gmail.com>
2020-06-08 00:55:12 +0300

    Составить цепочку слов

---
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2209/Example.java
@@ -0,0 +1,78 @@
+package com.javarush.task.task22.task2209;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+
+/*
+Составить цепочку слов
+*/
+public class Example {
+    public static void main(String[] args) throws IOException {
+        String fileName = new Scanner(System.in).nextLine();
+
+        List<String> words = new ArrayList<>();
+        try (Scanner scanner = new Scanner(new File(fileName))) {
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+
+                if (line != null) {
+                    String[] ww = line.split("\\s+");
+
+                    words.addAll(Arrays.asList(ww));
+                }
+            }
+        }
+
+        StringBuilder result = getLine(words.toArray(new String[0]));
+        System.out.println(result.toString());
+    }
+
+    public static StringBuilder getLine(String... words) {
+        if (words == null) {
+            return new StringBuilder();
+        }
+
+        List<String> result = new ArrayList<>();
+        fill(Arrays.asList(words), result);
+
+        return new StringBuilder(String.join(" ", result));
+    }
+
+    private static boolean fill(List<String> source, List<String> target) {
+        if (source.isEmpty()) {
+            return true;
+        }
+
+        for (String word : source) {
+            if (target.isEmpty()) {
+                if (process(new ArrayList<>(source), target, word)) {
+                    return true;
+                }
+            } else {
+                String lastWord = target.get(target.size() - 1);
+                if (lastWord.toLowerCase().charAt(lastWord.length() - 1) == word.toLowerCase().charAt(0)) {
+                    if (process(new ArrayList<>(source), target, word)) {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private static boolean process(List<String> source, List<String> target, String word) {
+        source.remove(word);
+
+        List<String> target2 = new ArrayList<>(target);
+        target2.add(word);
+
+        if (fill(source, target2)) {
+            target.clear();
+            target.addAll(target2);
+            return true;
+        }
+        return false;
+    }
+}
