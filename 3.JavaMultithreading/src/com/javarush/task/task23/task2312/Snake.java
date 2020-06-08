80a26bd7d77a8f7e1f68e4a0d8b38d44ac3e310d
bltproof <aka.vincente.vega@gmail.com>
2020-06-08 18:47:40 +0300

    Змейка(8)

--- 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Snake.java
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Snake.java
@@ -1,11 +1,19 @@
 package com.javarush.task.task23.task2312;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Snake {
     private List<SnakeSection> sections;
     private boolean isAlive;
     private SnakeDirection direction;
+
+    public Snake(int x, int y) {
+        sections = new ArrayList<>();
+        SnakeSection head = new SnakeSection(x, y);
+        sections.add(head);
+        isAlive = true;
+    }
 
     public List<SnakeSection> getSections() {
         return sections;
@@ -21,5 +29,17 @@ public class Snake {
 
     public void setDirection(SnakeDirection direction) {
         this.direction = direction;
+    }
+
+    public int getX() {
+        return this.sections.get(0).getX();
+    }
+
+    public int getY() {
+        return this.sections.get(0).getY();
+    }
+
+    public void move() {
+
     }
 }
