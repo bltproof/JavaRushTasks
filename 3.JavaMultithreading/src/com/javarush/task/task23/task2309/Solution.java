b424155a61ed884c2b04ee948b6674cb67d699ba
bltproof <aka.vincente.vega@gmail.com>
2020-06-10 11:02:21 +0300

    Анонимность иногда так приятна!

--- 3.JavaMultithreading/src/com/javarush/task/task23/task2309/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2309/Solution.java
@@ -1,7 +1,9 @@
 package com.javarush.task.task23.task2309;
 
-import com.javarush.task.task23.task2309.vo.NamedItem;
+import com.javarush.task.task23.task2309.vo.*;
 
+import javax.jws.soap.SOAPBinding;
+import java.util.ArrayList;
 import java.util.List;
 
 /* 
@@ -12,6 +14,9 @@ public class Solution {
         Solution solution = new Solution();
         print(solution.getUsers());
         print(solution.getLocations());
+        print(solution.getServers());
+        print(solution.getSubjects());
+        print(solution.getSubscriptions());
     }
 
     public static void print(List list) {
@@ -20,5 +25,52 @@ public class Solution {
             NamedItem item = (NamedItem) obj;
             System.out.println(String.format(format, item.getId(), item.getName(), item.getDescription()));
         }
+    }
+
+    public List<User> getUsers() {
+        return new AbstractDbSelectExecutor<User>() {
+            @Override
+            public String getQuery() {
+                return "SELECT * FROM USER";
+            }
+        }.execute();
+    }
+
+    public List<Location> getLocations() {
+        return new AbstractDbSelectExecutor<Location>() {
+            @Override
+            public String getQuery() {
+                return "SELECT * FROM LOCATION";
+            }
+        }.execute();
+    }
+
+    public List<Server> getServers() {
+        return new AbstractDbSelectExecutor<Server>() {
+            @Override
+            public String getQuery() {
+                return "SELECT * FROM SERVER";
+            }
+        }.execute();
+
+    }
+
+    public List<Subject> getSubjects() {
+        return new AbstractDbSelectExecutor<Subject>() {
+            @Override
+            public String getQuery() {
+                return "SELECT * FROM SUBJECT";
+            }
+        }.execute();
+
+    }
+
+    public List<Subscription> getSubscriptions() {
+        return new AbstractDbSelectExecutor<Subscription>() {
+            @Override
+            public String getQuery() {
+                return "SELECT * FROM SUBSCRIPTION";
+            }
+        }.execute();
     }
 }
