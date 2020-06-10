c75b816f105109da01c1dbbc9ac2269f73aab21b
bltproof <aka.vincente.vega@gmail.com>
2020-06-10 13:33:34 +0300

    Арканоид(3)

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Arkanoid.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Arkanoid.java
@@ -1,6 +1,30 @@
 package com.javarush.task.task24.task2413;
 
 public class Arkanoid {
+    private int height;
+    private int width;
+
+    public Arkanoid(int height, int width) {
+        this.height = height;
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
     public static void main(String[] args) {
 
     }
