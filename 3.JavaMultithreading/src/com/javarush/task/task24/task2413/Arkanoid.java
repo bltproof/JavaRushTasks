1c1a5d6b56d4bdc2eef2f04b4d06fe530cb57414
bltproof <aka.vincente.vega@gmail.com>
2020-06-10 13:38:34 +0300

    Арканоид(4)

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Arkanoid.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Arkanoid.java
@@ -1,12 +1,27 @@
 package com.javarush.task.task24.task2413;
 
+import java.util.List;
+
 public class Arkanoid {
     private int width;
     private int height;
 
+    private Ball ball;
+    private Stand stand;
+
+    private List<Brick> bricks;
+
     public Arkanoid(int width, int height) {
         this.height = height;
         this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
     }
 
     public int getWidth() {
@@ -17,12 +32,28 @@ public class Arkanoid {
         this.width = width;
     }
 
-    public int getHeight() {
-        return height;
+    public Ball getBall() {
+        return ball;
     }
 
-    public void setHeight(int height) {
-        this.height = height;
+    public void setBall(Ball ball) {
+        this.ball = ball;
+    }
+
+    public Stand getStand() {
+        return stand;
+    }
+
+    public void setStand(Stand stand) {
+        this.stand = stand;
+    }
+
+    public List<Brick> getBricks() {
+        return bricks;
+    }
+
+    public void setBricks(List<Brick> bricks) {
+        this.bricks = bricks;
     }
 
     public static void main(String[] args) {
