50d03c2b8d4fc925d3d94fec4840eed62085941d
bltproof <aka.vincente.vega@gmail.com>
2020-06-10 13:49:47 +0300

    Арканоид(7)

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Ball.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Ball.java
@@ -2,6 +2,10 @@ package com.javarush.task.task24.task2413;
 
 public class Ball extends BaseObject {
 
+    public Ball(double x, double y, double radius) {
+        super(x, y, radius);
+    }
+
     public void move() {
 
     }
--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Brick.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Brick.java
@@ -2,6 +2,10 @@ package com.javarush.task.task24.task2413;
 
 public class Brick extends BaseObject {
 
+    public Brick(double x, double y, double radius) {
+        super(x, y, radius);
+    }
+
     public void move() {
 
     }
