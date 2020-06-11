2e035448319be396b32839756f7e3a3ba2bc7341
ShaipovZA <z.shaipov@id-mt.ru>
2020-06-11 23:38:07 +0300

    Арканоид(13)

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Ball.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Ball.java
@@ -1,9 +1,20 @@
 package com.javarush.task.task24.task2413;
 
 public class Ball extends BaseObject {
+    private double speed;
+    private double direction;
+    private double dx;
+    private double dy;
+    private boolean isFrozen = true;
 
     public Ball(double x, double y, double radius) {
         super(x, y, radius);
+    }
+
+    public Ball(double x, double y, double speed, double direction) {
+        super(x, y, 1);
+        this.speed = speed;
+        this.direction = direction;
     }
 
     @Override
@@ -14,5 +25,37 @@ public class Ball extends BaseObject {
     @Override
     public void draw(Canvas canvas) {
 
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(double speed) {
+        this.speed = speed;
+    }
+
+    public double getDirection() {
+        return direction;
+    }
+
+    public void setDirection(double direction) {
+        this.direction = direction;
+    }
+
+    public double getDx() {
+        return dx;
+    }
+
+    public void setDx(double dx) {
+        this.dx = dx;
+    }
+
+    public double getDy() {
+        return dy;
+    }
+
+    public void setDy(double dy) {
+        this.dy = dy;
     }
 }
