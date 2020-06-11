1cedb0e610c7ecbe367d8c4935e7ffab927a8594
ShaipovZA <z.shaipov@id-mt.ru>
2020-06-11 23:50:01 +0300

    Арканоид(15)

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Ball.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Ball.java
@@ -34,6 +34,10 @@ public class Ball extends BaseObject {
         isFrozen = false;
     }
 
+    void checkRebound(int minx, int maxx, int miny, int maxy) {
+
+    }
+
     public double getSpeed() {
         return speed;
     }
@@ -48,6 +52,9 @@ public class Ball extends BaseObject {
 
     public void setDirection(double direction) {
         this.direction = direction;
+        double angle = Math.toRadians(direction);
+        dx = Math.cos(angle) * speed;
+        dy = -Math.sin(angle) * speed;
     }
 
     public double getDx() {
