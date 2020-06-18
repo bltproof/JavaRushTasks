639aecabad26d08a05b2bddc9ba025aa936ac14f
bltproof <aka.vincente.vega@gmail.com>
2020-06-18 03:42:48 +0300

    MVC(1)

---
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/model/FakeModel.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task36.task3608.model;
+
+import com.javarush.task.task36.task3608.bean.User;
+
+import java.util.Arrays;
+
+public class FakeModel implements Model {
+    private ModelData modelData = new ModelData();
+
+    @Override
+    public ModelData getModelData() {
+        return modelData;
+    }
+
+    @Override
+    public void loadUsers() {
+        modelData.setUsers(Arrays.asList(
+                new User("A", 1, 1),
+                new User("B", 2, 1)
+        ));
+    }
+}
