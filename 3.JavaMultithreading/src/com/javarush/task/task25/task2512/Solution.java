676a0538c9d0a915464036580bfeeacf86745809
bltproof <aka.vincente.vega@gmail.com>
2020-06-21 20:12:24 +0300

    Живем своим умом

---
+++ 3.JavaMultithreading/src/com/javarush/task/task25/task2512/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task25.task2512;
+
+/*
+Живем своим умом
+*/
+public class Solution implements Thread.UncaughtExceptionHandler {
+
+    @Override
+    public void uncaughtException(Thread t, Throwable e) {
+        if (e != null) {
+            t.interrupt();
+            uncaughtException(t, e.getCause());
+            System.out.println(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        new Solution().uncaughtException(
+                new Thread(), new Exception("ABC", new RuntimeException("DEF", new IllegalAccessException("GHI")))
+        );
+    }
+}
