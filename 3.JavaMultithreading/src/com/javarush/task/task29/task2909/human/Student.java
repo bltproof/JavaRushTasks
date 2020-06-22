743b4ffa74256da0d6b72dd4bcef614675543403
ShaipovZA <z.shaipov@id-mt.ru>
2020-06-22 19:06:44 +0300

    Рефакторинг(4)

--- 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/Student.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/Student.java
@@ -2,9 +2,8 @@ package com.javarush.task.task29.task2909.human;
 
 import java.util.Date;
 
-public class Student extends Human {
+public class Student extends UniversityPerson {
     private double averageGrade;
-    private String university;
     private Date beginningOfSession;
     private Date endOfSession;
     private int course;
@@ -19,14 +18,6 @@ public class Student extends Human {
     }
 
     public void learn() {
-    }
-
-    public String getUniversity() {
-        return university;
-    }
-
-    public void setUniversity(String university) {
-        this.university = university;
     }
 
     public void printData() {
