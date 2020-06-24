62d858116d9d422d58aedd002c53877bea60d0e7
ShaipovZA <z.shaipov@id-mt.ru>
2020-06-25 00:10:57 +0300

    Почитать в инете про медиану выборки

--- 3.JavaMultithreading/src/com/javarush/task/task26/task2601/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task26/task2601/Solution.java
@@ -1,48 +1,23 @@
 package com.javarush.task.task26.task2601;
 
 import java.util.*;
-import java.util.function.Function;
 
 /*
 Почитать в инете про медиану выборки
 */
-
-//13, 8, 15, 5, 17
-//13, 15, 17, 8, 5
 public class Solution {
 
     public static void main(String[] args) {
-        Integer[] arr = new Integer[]{13, 8, 15, 5, 17};
-        arr = sort(arr);
-        for (Integer i : arr) {
-            System.out.print(i + " ");
-        }
-    }
-
-    public static Integer medianSearch(Integer[] array) {
-        Arrays.sort(array);
-        if (array.length % 2 == 1) return array[array.length / 2];
-        return (array[array.length / 2 - 1] + array[array.length / 2]) / 2;
     }
 
     public static Integer[] sort(Integer[] array) {
         //implement logic here
-        List<Integer> list = new ArrayList<>(Arrays.asList(array));
-        int median = medianSearch(array);
+        Arrays.sort(array);
 
-        for (int i = 0; i < array.length; i++) {
-            array[i] = array[i] - median;
-            System.out.print(array[i] + " ");
-        }
-        System.out.println();
+        int median = array.length % 2 == 1 ? array[array.length / 2] : (array[array.length / 2] + array[array.length / 2 - 1]) / 2;
 
-        list.sort(Comparator.comparing(new Function<Integer, Integer>() {
-            @Override
-            public Integer apply(Integer integer) {
-                return Math.abs(integer - median);
-            }
-        }));
+        Arrays.sort(array, Comparator.comparingInt(o -> Math.abs(o - median)));
 
-        return list.toArray(new Integer[0]);
+        return array;
     }
 }
