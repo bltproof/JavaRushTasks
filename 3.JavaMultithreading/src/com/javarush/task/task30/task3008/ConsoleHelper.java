fae8c09d060e674328810328995b69dcee830db2
ShaipovZA <z.shaipov@id-mt.ru>
2020-06-26 04:04:53 +0300

    Чат(2)

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/ConsoleHelper.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/ConsoleHelper.java
@@ -1,5 +1,37 @@
 package com.javarush.task.task30.task3008;
 
-public class ConsoleHelper {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class ConsoleHelper {
+    private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void writeMessage(String message) {
+        System.out.println(message);
+    }
+
+    public static String readString() {
+        String text;
+        try {
+            text = reader.readLine();
+
+        } catch (IOException e) {
+            System.out.println("Произошла ошибка при попытке ввода текста. Попробуйте еще раз.");
+            return readString();
+        }
+        return text;
+    }
+
+    public static int readInt() {
+        int number;
+        try {
+            number = Integer.parseInt(readString());
+
+        } catch (NumberFormatException e) {
+            System.out.println("Произошла ошибка при попытке ввода числа. Попробуйте еще раз.");
+            return readInt();
+        }
+        return number;
+    }
 }
