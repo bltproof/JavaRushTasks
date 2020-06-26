f3c07917918c8c836354a1f2b23898fab610c208
ShaipovZA <z.shaipov@id-mt.ru>
2020-06-26 21:13:55 +0300

    Чат(5)

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Connection.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Connection.java
@@ -1,5 +1,51 @@
 package com.javarush.task.task30.task3008;
 
-public class Connection {
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.net.SocketAddress;
 
+public class Connection implements Closeable {
+    private final Socket socket;
+    private final ObjectInputStream in;
+    private final ObjectOutputStream out;
+
+    public Connection(Socket socket) throws Exception {
+        this.socket = socket;
+        this.out =  new ObjectOutputStream(socket.getOutputStream());
+        this.in =  new ObjectInputStream(socket.getInputStream());
+    }
+
+    public void send(Message message) throws IOException {
+        synchronized (out) {
+            out.writeObject(message);
+            out.flush();
+        }
+    }
+
+    public Message receive() throws IOException, ClassNotFoundException {
+        Message message;
+        synchronized (in) {
+            message = (Message) in.readObject();
+            return message;
+        }
+    }
+
+    public SocketAddress getRemoteSocketAddress() {
+        return socket.getRemoteSocketAddress();
+    }
+
+    public void close() throws IOException {
+        if (in != null) {
+            in.close();
+        }
+        if (out != null) {
+            out.close();
+        }
+        if (socket != null) {
+            socket.close();
+        }
+    }
 }
