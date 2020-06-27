9a93cecacd8276dad8a92bdc860d903745922255
ShaipovZA <z.shaipov@id-mt.ru>
2020-06-27 17:07:10 +0300

    Чат(16)

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
@@ -98,6 +98,40 @@ public class Client {
                 Client.this.notify();
             }
         }
+
+        protected void clientHandshake() throws IOException, ClassNotFoundException {
+            while (true) {
+                if (connection.receive().getType() == MessageType.NAME_REQUEST) {
+                    connection.send(new Message(MessageType.USER_NAME, getUserName()));
+
+                } else if (connection.receive().getType() == MessageType.NAME_ACCEPTED) {
+                    notifyConnectionStatusChanged(true);
+                    return;
+
+                } else {
+                    throw new IOException("Unexpected message type");
+                }
+            }
+        }
+
+        protected void clientMainLoop() throws IOException, ClassNotFoundException {
+            while (true) {
+                Message message = connection.receive();
+
+                if (message.getType() == MessageType.TEXT) {
+                    processIncomingMessage(message.getData());
+
+                } else if (message.getType() == MessageType.USER_ADDED) {
+                    informAboutAddingNewUser(message.getData());
+
+                } else if (message.getType() == MessageType.USER_REMOVED) {
+                    informAboutDeletingNewUser(message.getData());
+
+                } else {
+                    throw new IOException("Unexpected MessageType");
+                }
+            }
+        }
     }
 
     public static void main(String[] args) {
