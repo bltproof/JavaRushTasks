52d84b5be6e47cba682a69361f89d030e1663548
ShaipovZA <z.shaipov@id-mt.ru>
2020-06-28 00:23:49 +0300

    Генератор паролей

--- 4.JavaCollections/src/com/javarush/task/task32/task3204/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task32/task3204/Solution.java
@@ -14,13 +14,13 @@ public class Solution {
 
     public static ByteArrayOutputStream getPassword() {
         ByteArrayOutputStream out = new ByteArrayOutputStream();
-        String alphabetLowerCase = "abcdefghijklmnopqrstuvwxyz";
-        String alphabetUpperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String lowerCaseAlphabet = "abcdefghijklmnopqrstuvwxyz";
+        String upperCaseAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         String numbers = "1234567890";
 
         for (int i = 0; i < 8; i++) {
-            out.write(alphabetLowerCase.charAt(new Random().nextInt(alphabetLowerCase.length())));
-            out.write(alphabetUpperCase.charAt(new Random().nextInt(alphabetUpperCase.length())));
+            out.write(lowerCaseAlphabet.charAt(new Random().nextInt(lowerCaseAlphabet.length())));
+            out.write(upperCaseAlphabet.charAt(new Random().nextInt(upperCaseAlphabet.length())));
             if (out.size() == 8) return out;
             out.write(numbers.charAt(new Random().nextInt(numbers.length())));
         }
