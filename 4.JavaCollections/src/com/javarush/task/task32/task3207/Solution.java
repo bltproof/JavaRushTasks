0ed2589e985e24b593045fde6f2ff3c66178c507
ShaipovZA <z.shaipov@id-mt.ru>
2020-06-30 19:21:21 +0300

    К серверу по RMI

--- 4.JavaCollections/src/com/javarush/task/task32/task3207/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task32/task3207/Solution.java
@@ -1,9 +1,6 @@
 package com.javarush.task.task32.task3207;
 
-import java.rmi.AlreadyBoundException;
-import java.rmi.NotBoundException;
-import java.rmi.Remote;
-import java.rmi.RemoteException;
+import java.rmi.*;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
@@ -33,9 +30,9 @@ public class Solution {
     public static void main(String[] args) {
         // Pretend we're starting an RMI server as the main thread
         Remote stub = null;
+        final DoubleStringImpl service = new DoubleStringImpl();
         try {
             registry = LocateRegistry.createRegistry(2099);
-            final DoubleStringImpl service = new DoubleStringImpl();
 
             stub = UnicastRemoteObject.exportObject(service, 0);
             registry.bind(UNIC_BINDING_NAME, stub);
@@ -47,5 +44,12 @@ public class Solution {
 
         // Start the client
         CLIENT_THREAD.start();
+
+        /*try { //Для корректного завершения
+            Thread.sleep(200);
+            UnicastRemoteObject.unexportObject(service, true);
+        } catch (InterruptedException | NoSuchObjectException e) {
+            e.printStackTrace();
+        }*/
     }
 }
