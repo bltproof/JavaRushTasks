8b2e444311a5d5c5103528e6548e58210e03c5d7
ShaipovZA <z.shaipov@id-mt.ru>
2020-06-30 18:18:16 +0300

    Дженерики для создания прокси-объекта

---
+++ 4.JavaCollections/src/com/javarush/task/task32/task3206/Solution.java
@@ -0,0 +1,35 @@
+package com.javarush.task.task32.task3206;
+
+import java.lang.reflect.Proxy;
+
+/*
+Дженерики для создания прокси-объекта
+*/
+public class Solution {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        test(solution.getProxy(Item.class));                        //true false false
+        test(solution.getProxy(Item.class, Small.class));           //true false true
+        test(solution.getProxy(Item.class, Big.class, Small.class));//true true true
+        test(solution.getProxy(Big.class, Small.class));            //true true true т.к. Big наследуется от Item
+        test(solution.getProxy(Big.class));                         //true true false т.к. Big наследуется от Item
+    }
+
+
+    private static void test(Object proxy) {
+        boolean isItem = proxy instanceof Item;
+        boolean isBig = proxy instanceof Big;
+        boolean isSmall = proxy instanceof Small;
+
+        System.out.format("%b %b %b\n", isItem, isBig, isSmall);
+    }
+
+    public <T extends Item> T getProxy(Class<T> item, Class... interfaces) {
+        Class[] interfacesCopy = new Class[interfaces.length + 1];
+        interfacesCopy[0] = item;
+        System.arraycopy(interfaces, 0, interfacesCopy, 1, interfaces.length);
+
+        return (T) Proxy.newProxyInstance(this.getClass().getClassLoader(), interfacesCopy, new ItemInvocationHandler());
+    }
+}
