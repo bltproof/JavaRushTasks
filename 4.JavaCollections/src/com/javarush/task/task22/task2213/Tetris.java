886c9eb506048aabbf462a78bf1a7a9baf3af946
ShaipovZA <z.shaipov@id-mt.ru>
2020-06-30 23:17:32 +0300

    Тетрис(5)

--- 4.JavaCollections/src/com/javarush/task/task22/task2213/Tetris.java
+++ 4.JavaCollections/src/com/javarush/task/task22/task2213/Tetris.java
@@ -3,8 +3,34 @@ package com.javarush.task.task22.task2213;
 public class Tetris {
     private Field field;
     private Figure figure;
+    static Tetris game;
+
+    public Field getField() {
+        return field;
+    }
+
+    public void setField(Field field) {
+        this.field = field;
+    }
+
+    public Figure getFigure() {
+        return figure;
+    }
+
+    public void setFigure(Figure figure) {
+        this.figure = figure;
+    }
+
+    public void run() {
+
+    }
+
+    public void step() {
+        
+    }
 
     public static void main(String[] args) {
-
+        game = new Tetris();
+        game.run();
     }
 }
