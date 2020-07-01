9c7c3d1687942b583668aa15f5dab3a3d478a6c7
ShaipovZA <z.shaipov@id-mt.ru>
2020-07-02 00:10:59 +0300

    Игра Racer (2/28)

--- 8.Games/src/com/javarush/games/racer/RacerGame.java
+++ 8.Games/src/com/javarush/games/racer/RacerGame.java
@@ -5,10 +5,25 @@ import com.javarush.engine.cell.*;
 public class RacerGame extends Game {
     public static final int WIDTH = 64;
     public static final int HEIGHT = 64;
+    public static final int CENTER_X = WIDTH / 2;
+    public static final int ROADSIDE_WIDTH = 14;
 
     @Override
     public void initialize() {
-        setScreenSize(WIDTH, HEIGHT);
         showGrid(false);
+        setScreenSize(WIDTH, HEIGHT);
+        createGame();
+    }
+
+    private void createGame() {
+        drawScene();
+    }
+
+    private void drawScene() {
+        drawField();
+    }
+
+    private void drawField() {
+
     }
 }
