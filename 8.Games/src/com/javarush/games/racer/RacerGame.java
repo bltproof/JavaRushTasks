de7fdf6cfea2491194234f28ba3bebce3c8354ba
ShaipovZA <z.shaipov@id-mt.ru>
2020-07-02 00:53:11 +0300

    Игра Racer (3/28)

--- 8.Games/src/com/javarush/games/racer/RacerGame.java
+++ 8.Games/src/com/javarush/games/racer/RacerGame.java
@@ -24,6 +24,16 @@ public class RacerGame extends Game {
     }
 
     private void drawField() {
-
+        for (int x = 0; x < WIDTH; x++) {
+            for (int y = 0; y < HEIGHT; y++) {
+                if (x == CENTER_X) {
+                    setCellColor(CENTER_X, y, Color.WHITE);
+                } else if (x >= ROADSIDE_WIDTH && x < (WIDTH - ROADSIDE_WIDTH)) {
+                    setCellColor(x, y, Color.DIMGREY);
+                } else {
+                    setCellColor(x, y, Color.GREEN);
+                }
+            }
+        }
     }
 }
