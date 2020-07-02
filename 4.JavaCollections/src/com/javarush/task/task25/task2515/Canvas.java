ac11f6bdbb613f479385b8c166d1974d7d26aa19
ShaipovZA <z.shaipov@id-mt.ru>
2020-07-02 20:00:59 +0300

    Space (9)

--- 4.JavaCollections/src/com/javarush/task/task25/task2515/Canvas.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/Canvas.java
@@ -45,4 +45,25 @@ public class Canvas {
             }
         }
     }
+
+    public void clear() {
+        this.matrix = new char[height][width];
+    }
+
+    public void print() {
+        System.out.println();
+
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                System.out.print(" ");
+                System.out.print(matrix[i][j]);
+                System.out.print(" ");
+            }
+
+            System.out.println();
+        }
+
+        System.out.println();
+        System.out.println();
+    }
 }
