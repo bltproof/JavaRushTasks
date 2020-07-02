0ae0efe254203c1feeafce5d9da9aea98b0ebebc
ShaipovZA <z.shaipov@id-mt.ru>
2020-07-02 23:39:46 +0300

    Space (18)

--- 4.JavaCollections/src/com/javarush/task/task25/task2515/Space.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/Space.java
@@ -84,7 +84,6 @@ public class Space {
      * Двигаем все объекты игры
      */
     public void moveAllItems() {
-        //нужно получить список всех игрвых объектов и у каждого вызвать метод move().
         for (BaseObject object : getAllItems()) {
             object.move();
         }
@@ -94,14 +93,10 @@ public class Space {
      * Метод возвращает общий список, который содержит все объекты игры
      */
     public List<BaseObject> getAllItems() {
-        //нужно создать новый список и положить в него все игровые объекты.
-        List<BaseObject> list = new ArrayList<>();
-
+        ArrayList<BaseObject> list = new ArrayList<BaseObject>(ufos);
+        list.add(ship);
         list.addAll(bombs);
         list.addAll(rockets);
-        list.addAll(ufos);
-        list.add(ship);
-
         return list;
     }
 
@@ -109,12 +104,13 @@ public class Space {
      * Создаем новый НЛО. 1 раз на 10 вызовов.
      */
     public void createUfo() {
-        //тут нужно создать новый НЛО.
-        //1 раз за 10 вызовов метода.
         if (ufos.size() > 0) return;
 
-        if (Math.random() * 10 == 0) {
-            ufos.add(new Ufo(Math.random() * width, Math.random() * height / 2));
+        int random10 = (int) (Math.random() * 10);
+        if (random10 == 0) {
+            double x = Math.random() * width;
+            double y = Math.random() * height / 2;
+            ufos.add(new Ufo(x, y));
         }
     }
 
@@ -124,7 +120,6 @@ public class Space {
      * б) падение ниже края игрового поля (бомба умирает)
      */
     public void checkBombs() {
-        //тут нужно проверить все возможные столкновения для каждой бомбы.
         for (Bomb bomb : bombs) {
             if (ship.isIntersect(bomb)) {
                 ship.die();
@@ -142,7 +137,6 @@ public class Space {
      * б) вылет выше края игрового поля (ракета умирает)
      */
     public void checkRockets() {
-        //тут нужно проверить все возможные столкновения для каждой ракеты.
         for (Rocket rocket : rockets) {
             for (Ufo ufo : ufos) {
                 if (ufo.isIntersect(rocket)) {
@@ -160,16 +154,19 @@ public class Space {
      * Удаляем умершие объекты (бомбы, ракеты, НЛО) из списков
      */
     public void removeDead() {
-        //тут нужно удалить все умершие объекты из списков.
-        //Кроме космического корабля - по нему определяем идет еще игра или нет.
-        for (Bomb bomb : new ArrayList<>(bombs)) {
-            if (!bomb.isAlive()) bombs.remove(bomb);
+        for (BaseObject object : new ArrayList<BaseObject>(bombs)) {
+            if (!object.isAlive())
+                bombs.remove(object);
         }
-        for (Rocket rocket : new ArrayList<>(rockets)) {
-            if (!rocket.isAlive()) rockets.remove(rocket);
+
+        for (BaseObject object : new ArrayList<BaseObject>(rockets)) {
+            if (!object.isAlive())
+                rockets.remove(object);
         }
-        for (Ufo ufo : new ArrayList<>(ufos)) {
-            if (!ufo.isAlive()) ufos.remove(ufo);
+
+        for (BaseObject object : new ArrayList<BaseObject>(ufos)) {
+            if (!object.isAlive())
+                ufos.remove(object);
         }
     }
 
@@ -179,7 +176,26 @@ public class Space {
      * б) отрисовываем все объекты на холст.
      */
     public void draw(Canvas canvas) {
-        //тут нужно отрисовать все объекты игры
+        //draw game
+        for (int i = 0; i < width + 2; i++) {
+            for (int j = 0; j < height + 2; j++) {
+                canvas.setPoint(i, j, '.');
+            }
+        }
+
+        for (int i = 0; i < width + 2; i++) {
+            canvas.setPoint(i, 0, '-');
+            canvas.setPoint(i, height + 1, '-');
+        }
+
+        for (int i = 0; i < height + 2; i++) {
+            canvas.setPoint(0, i, '|');
+            canvas.setPoint(width + 1, i, '|');
+        }
+
+        for (BaseObject object : getAllItems()) {
+            object.draw(canvas);
+        }
     }
 
 
