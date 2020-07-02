63abbddde55ebc5bcc2f6ed6ce7d1028b9e6cc3f
ShaipovZA <z.shaipov@id-mt.ru>
2020-07-02 20:10:18 +0300

    Space (11)

--- 4.JavaCollections/src/com/javarush/task/task25/task2515/Rocket.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/Rocket.java
@@ -1,18 +1,27 @@
 package com.javarush.task.task25.task2515;
 
+/**
+ * Класс для объектов-ракета
+ */
 public class Rocket extends BaseObject {
 
-    public Rocket(double x, double y, double raduis) {
-        super(x, y, raduis);
+    public Rocket(double x, double y) {
+        super(x, y, 1);
     }
 
-    @Override
-    public void move() {
-
-    }
-
+    /**
+     * Метод рисует свой объект на "канвасе".
+     */
     @Override
     public void draw(Canvas canvas) {
+        canvas.setPoint(x, y, 'R');
+    }
 
+    /**
+     * Двигаем себя вверх на один ход.
+     */
+    @Override
+    public void move() {
+        y--;
     }
 }
