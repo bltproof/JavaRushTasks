f1c8b5fa5560e094b299e0686be5a90ddf27d5bc
ShaipovZA <z.shaipov@id-mt.ru>
2020-07-05 03:14:45 +0300

    Кеш на основании SoftReference

---
+++ 4.JavaCollections/src/com/javarush/task/task34/task3413/SoftCache.java
@@ -0,0 +1,42 @@
+package com.javarush.task.task34.task3413;
+
+import java.lang.ref.SoftReference;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class SoftCache {
+    private Map<Long, SoftReference<AnyObject>> cacheMap = new ConcurrentHashMap<>();
+
+    public AnyObject get(Long key) {
+        SoftReference<AnyObject> softReference = cacheMap.get(key);
+
+        //напишите тут ваш код
+        if (softReference == null) return null;
+        AnyObject anyObject = softReference.get();
+        softReference.clear();
+
+        return anyObject;
+    }
+
+    public AnyObject put(Long key, AnyObject value) {
+        SoftReference<AnyObject> softReference = cacheMap.put(key, new SoftReference<>(value));
+
+        //напишите тут ваш код
+        if (softReference == null) return null;
+        AnyObject anyObject = softReference.get();
+        softReference.clear();
+
+        return anyObject;
+    }
+
+    public AnyObject remove(Long key) {
+        SoftReference<AnyObject> softReference = cacheMap.remove(key);
+
+        //напишите тут ваш код
+        if (softReference == null) return null;
+        AnyObject anyObject = softReference.get();
+        softReference.clear();
+
+        return anyObject;
+    }
+}
