0d0053cdd26e62b6b91d9929206831c976fd5770
ShaipovZA <z.shaipov@id-mt.ru>
2020-07-11 01:24:30 +0300

    ClassLoader - что это такое?

--- 4.JavaCollections/src/com/javarush/task/task35/task3507/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task35/task3507/Solution.java
@@ -1,10 +1,11 @@
 package com.javarush.task.task35.task3507;
 
 import java.io.File;
-import java.lang.reflect.Constructor;
+import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.nio.file.Path;
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 /* 
@@ -17,55 +18,38 @@ public class Solution {
     }
 
     public static Set<? extends Animal> getAllAnimals(String pathToAnimals) {
-        File[] files = new File(pathToAnimals).listFiles();
+        Set<Animal> result = new HashSet<>();
+        File[] listOfFiles = new File(pathToAnimals).listFiles();
 
-        ClassLoader classLoader = new ClassLoader() {
-            @Override
-            protected Class<?> findClass(String name) {
-                byte[] bytes = new byte[0];
-                try {
-                    bytes = Files.readAllBytes(Paths.get(name));
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-                return defineClass(name, bytes, 0, bytes.length);
+        class MyClassLoader extends ClassLoader {
+            private String pathtobin;
+
+            public MyClassLoader(String pathtobin) {
+                super();
+                this.pathtobin = pathtobin;
             }
-        };
 
-        Class clazz = null;
-        Set<Animal> resultSet = new HashSet<>();
+            private Class load(Path path) throws IOException {
+                byte b[] = Files.readAllBytes(path);
+                return defineClass(null, b, 0, b.length);
+            }
+        }
 
-        if (files != null) {
-            for (File f : files) {
+        for (File f : Objects.requireNonNull(listOfFiles)) {
+            if (f.isFile() && f.getName().endsWith(".class")) {
+
+                MyClassLoader loader = new MyClassLoader(f.getClass().toString());
+
                 try {
-                    boolean isDefaultConstructor = false;
-                    boolean isAnimalInterface = false;
+                    Class clazz = loader.load(f.toPath());
+                    Animal inst = (Animal) clazz.newInstance();
+                    result.add(inst);
 
-                    clazz = classLoader.loadClass(f.getName());
-                    Class[] interfaces = clazz.getInterfaces();
-
-                    Constructor[] constructor = clazz.getConstructors();
-
-                    for (Constructor c : constructor) {
-                        if (!c.isVarArgs() && c.getParameterCount() == 0) {
-                            isDefaultConstructor = true;
-                        }
-                    }
-                    for (Class c : interfaces) {
-                        if (c.getSimpleName().equals("Animal")) {
-                            isAnimalInterface = true;
-                        }
-                    }
-
-                    if (isAnimalInterface && isDefaultConstructor) {
-                        resultSet.add((Animal) clazz.newInstance());
-                    }
-
-                } catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
+                } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
         }
-        return resultSet;
+        return result;
     }
 }
