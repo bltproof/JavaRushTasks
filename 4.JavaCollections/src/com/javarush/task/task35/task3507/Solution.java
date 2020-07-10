e8edaabb3b9fdd7fa0d317ea090600c0487c7a68
ShaipovZA <z.shaipov@id-mt.ru>
2020-07-11 00:55:47 +0300

    ClassLoader - что это такое?

--- 4.JavaCollections/src/com/javarush/task/task35/task3507/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task35/task3507/Solution.java
@@ -1,5 +1,6 @@
 package com.javarush.task.task35.task3507;
 
+import java.io.File;
 import java.lang.reflect.Constructor;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -16,34 +17,31 @@ public class Solution {
     }
 
     public static Set<? extends Animal> getAllAnimals(String pathToAnimals) {
-        String[] classes = Paths.get(pathToAnimals.substring(1)).toFile().list();
+        File[] files = new File(pathToAnimals).listFiles();
 
         ClassLoader classLoader = new ClassLoader() {
             @Override
             protected Class<?> findClass(String name) {
-                String newName = "";
                 byte[] bytes = new byte[0];
                 try {
-                    String path = pathToAnimals.substring(1) + "/" + name;
-                    bytes = Files.readAllBytes(Paths.get(path));
-                    newName = path.substring(path.indexOf("com"), path.indexOf(".class")).replace('/', '.');
+                    bytes = Files.readAllBytes(Paths.get(name));
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
-                return defineClass(newName, bytes, 0, bytes.length);
+                return defineClass(name, bytes, 0, bytes.length);
             }
         };
 
         Class clazz = null;
         Set<Animal> resultSet = new HashSet<>();
 
-        if (classes != null) {
-            for (String s : classes) {
+        if (files != null) {
+            for (File f : files) {
                 try {
                     boolean isDefaultConstructor = false;
                     boolean isAnimalInterface = false;
 
-                    clazz = classLoader.loadClass(s);
+                    clazz = classLoader.loadClass(f.getName());
                     Class[] interfaces = clazz.getInterfaces();
 
                     Constructor[] constructor = clazz.getConstructors();
