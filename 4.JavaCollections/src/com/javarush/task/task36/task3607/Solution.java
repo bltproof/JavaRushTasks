917fce56057439e78d2afab24df784dad28aba73
ShaipovZA <z.shaipov@id-mt.ru>
2020-07-18 23:26:53 +0300

    Найти класс по описанию Ӏ Java Collections: 6 уровень, 10 лекция

--- 4.JavaCollections/src/com/javarush/task/task36/task3607/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task36/task3607/Solution.java
@@ -1,9 +1,7 @@
 package com.javarush.task.task36.task3607;
 
 
-import java.lang.reflect.Constructor;
-import java.util.Collections;
-import java.util.Queue;
+import java.util.concurrent.DelayQueue;
 
 /*
 Найти класс по описанию Ӏ Java Collections: 6 уровень, 10 лекция
@@ -14,21 +12,6 @@ public class Solution {
     }
 
     public static Class getExpectedClass() {
-        Class[] classes = Collections.class.getDeclaredClasses();
-
-        for (Class clazz : classes) {
-            if (Queue.class.isAssignableFrom(clazz)) {
-                Constructor constructor = null;
-                try {
-                    constructor = clazz.getDeclaredConstructor();
-                    constructor.setAccessible(true);
-                    return clazz;
-
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-        return null;
+        return DelayQueue.class;
     }
 }
