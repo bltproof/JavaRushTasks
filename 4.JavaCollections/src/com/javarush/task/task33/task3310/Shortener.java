a3ed5b109b8e13bdfc7c6555a757260cd4500a11
ShaipovZA <z.shaipov@id-mt.ru>
2020-07-19 16:44:55 +0300

    Shortener (3)

---
+++ 4.JavaCollections/src/com/javarush/task/task33/task3310/Shortener.java
@@ -0,0 +1,28 @@
+package com.javarush.task.task33.task3310;
+
+import com.javarush.task.task33.task3310.strategy.StorageStrategy;
+
+public class Shortener {
+    private Long lastId;
+    private StorageStrategy storageStrategy;
+
+    public Shortener(StorageStrategy storageStrategy) {
+        this.lastId = 0L;
+        this.storageStrategy = storageStrategy;
+    }
+
+    public synchronized Long getId(String string) {
+        if (storageStrategy.containsValue(string)) {
+            return storageStrategy.getKey(string);
+        } else {
+            lastId++;
+            storageStrategy.put(lastId, string);
+        }
+
+        return lastId;
+    }
+
+    public synchronized String getString(Long id) {
+        return storageStrategy.getValue(id);
+    }
+}
