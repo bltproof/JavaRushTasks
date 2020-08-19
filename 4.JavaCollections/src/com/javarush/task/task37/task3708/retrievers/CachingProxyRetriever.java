d55a0867a1ca37b06a68a4fa0ba0e7f1d883fa8e
bltproof <aka.vincente.vega@gmail.com>
2020-08-19 14:36:21 +0300

    Кеширующий Proxy

---
+++ 4.JavaCollections/src/com/javarush/task/task37/task3708/retrievers/CachingProxyRetriever.java
@@ -0,0 +1,26 @@
+package com.javarush.task.task37.task3708.retrievers;
+
+import com.javarush.task.task37.task3708.cache.LRUCache;
+import com.javarush.task.task37.task3708.storage.Storage;
+
+public class CachingProxyRetriever implements Retriever {
+    private OriginalRetriever originalRetriever;
+    private LRUCache<Long, Object> lruCache;
+
+    public CachingProxyRetriever(Storage storage) {
+        this.originalRetriever = new OriginalRetriever(storage);
+        this.lruCache = new LRUCache<>(10);
+    }
+
+    @Override
+    public Object retrieve(long id) {
+        Object cache = lruCache.find(id);
+
+        if (cache == null) {
+            cache = originalRetriever.retrieve(id);
+            lruCache.set(id, cache);
+        }
+
+        return lruCache.find(id);
+    }
+}
