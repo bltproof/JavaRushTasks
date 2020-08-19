b3fc3f0b476b3770c47748fc13432983fba22cb8
bltproof <aka.vincente.vega@gmail.com>
2020-08-20 02:13:17 +0300

    Фасад

--- 4.JavaCollections/src/com/javarush/task/task37/task3711/Computer.java
+++ 4.JavaCollections/src/com/javarush/task/task37/task3711/Computer.java
@@ -1,9 +1,9 @@
 package com.javarush.task.task37.task3711;
 
 public class Computer {
-    CPU cpu = new CPU();
-    HardDrive hardDrive = new HardDrive();
-    Memory memory = new Memory();
+    private CPU cpu = new CPU();
+    private HardDrive hardDrive = new HardDrive();
+    private Memory memory = new Memory();
 
     public void run() {
         cpu.calculate();
