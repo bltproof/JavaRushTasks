04de00df2bf6af1ef13c35a0d92a431c53ae7bcf
ShaipovZA <z.shaipov@id-mt.ru>
2020-08-26 12:48:49 +0300

    Древний Рим

---
+++ 4.JavaCollections/src/com/javarush/task/task37/task3714/Solution.java
@@ -0,0 +1,68 @@
+package com.javarush.task.task37.task3714;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/* 
+Древний Рим
+*/
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Input a roman number to be converted to decimal: ");
+        String romanString = bufferedReader.readLine();
+        System.out.println("Conversion result equals " + romanToInteger(romanString));
+    }
+
+    public static int romanToInteger(String s) {
+        String romanNumeral = s.toUpperCase();
+        int result = 0;
+
+        List<RomanNumeral> romanNumerals = RomanNumeral.getReverseSortedValues();
+
+        int i = 0;
+
+        while ((romanNumeral.length() > 0) && (i < romanNumerals.size())) {
+            RomanNumeral symbol = romanNumerals.get(i);
+            if (romanNumeral.startsWith(symbol.name())) {
+                result += symbol.getValue();
+                romanNumeral = romanNumeral.substring(symbol.name().length());
+            } else {
+                i++;
+            }
+        }
+
+        if (romanNumeral.length() > 0) {
+            throw new IllegalArgumentException(s + " cannot be converted to a Roman Numeral");
+        }
+
+        return result;
+    }
+
+    public enum RomanNumeral {
+        I(1), IV(4), V(5), IX(9), X(10),
+        XL(40), L(50), XC(90), C(100),
+        CD(400), D(500), CM(900), M(1000);
+
+        private int value;
+
+        RomanNumeral(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
+
+        public static List<RomanNumeral> getReverseSortedValues() {
+            return Arrays.stream(values())
+                    .sorted(Comparator.comparing((RomanNumeral e) -> e.value).reversed())
+                    .collect(Collectors.toList());
+        }
+    }
+}
