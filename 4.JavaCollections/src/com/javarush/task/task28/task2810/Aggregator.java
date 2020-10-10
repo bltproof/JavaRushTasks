ef01bf86f088bbd7e47197bfbc75153832acbc7b
bltproof <aka.vincente.vega@gmail.com>
2020-10-10 11:17:43 +0300

    Aggregator (2)

--- 4.JavaCollections/src/com/javarush/task/task28/task2810/Aggregator.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/Aggregator.java
@@ -1,7 +1,17 @@
 package com.javarush.task.task28.task2810;
 
+import com.javarush.task.task28.task2810.model.Provider;
+import com.javarush.task.task28.task2810.model.Strategy;
+
 public class Aggregator {
     public static void main(String[] args) {
-
+        Provider provider = new Provider(new Strategy() {
+            @Override
+            public void execute() {
+                System.out.println("execute");
+            }
+        });
+        Controller controller = new Controller(provider);
+        System.out.println(controller);
     }
 }
